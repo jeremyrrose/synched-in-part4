a2562052789b21950dcf7ad06426bea3db39ad2d
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-04-02 23:49:08 -0400

    favorites solution

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+
 export default function Header (props) {
     return (
-        <div className="container-fluid d-flex justify-content-start p-2 m-2 bg-primary" style={{color: "white"}}>
-            <div className="logo" style={{fontWeight: "bold", fontSize: "24px"}}>SynchedIn</div>
+        <div className="container-fluid d-flex justify-content-start align-items-center p-2 m-2 bg-primary" style={{color: "white"}}>
+            <div className="logo">
+                <Link 
+                    style={{fontWeight: "bold", fontSize: "24px", color:"white", textDecoration: "none"}}
+                    to="/" 
+                    >SynchedIn</Link>
+            </div>
+            <Link 
+                className="ms-4" 
+                style={{color:"white", textDecoration: "none"}} 
+                to="/favorites"
+                >My Favorites</Link>
         </div>
     )
 }
